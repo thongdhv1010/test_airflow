92be8332720e2922f9bf13426392fbd683323b84
nvt <nguyenvanthong@gapo.com.vn>
2020-11-13 08:46:37 +0700

    add cassandra db

---
+++ create_cassandra_db.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+               ..
+              ( '`<
+               )(
+        ( ----'  '.
+        (         ;
+         (_______,' 
+    ~^~^~^~^~^~^~^~^~^~^~
+    Author: nvt
+    Date Created: 13/11/2020
+"""
+
+import os
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+
+auth_provider = PlainTextAuthProvider(username=os.getenv('CASSANDRA_USER'),
+                                      password=os.getenv('CASSANDRA_PWD'))
+cluster = Cluster(os.getenv('CASSANDRA_HOST').strip().split(','),
+                  auth_provider=auth_provider,
+                  protocol_version=3)
+session = cluster.connect(os.getenv('CASSANDRA_KEYSPACE'))
+
+session.execute("""
+    CREATE TABLE IF NOT EXISTS aggregate_action(
+    id uuid,
+    user_id int,
+    number_comment int,
+    number_like int,
+    start_time timestamp,
+    PRIMARY KEY (user_id, start_time)
+   );
+   """)
